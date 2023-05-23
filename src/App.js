7ff2398cb328d0bdaaf33fefb42104e22e8a6a9e
William Moses <wmoses5k@gmail.com>
2023-05-23 18:57:03 +0100

    added 404.html

--- src/App.js
+++ src/App.js
@@ -13,21 +13,13 @@ function App() {
     <Router>
       <Navbar />
         <Routes>
-          <Route path="/" element={<Home/>} />
+          <Route path="/Portfolio" element={<Home/>} />
           <Route path="/Home" element={<Home/>} />
           <Route path="/PortfolioPage" element={<PortfolioPage/>} />
           <Route path="/About" element={<About/>} />
           <Route path="/PCG" element={<PCG/>} />
         </Routes>
     </Router>
-
-    {/* <div class="wrapper">
-    <span>
-      <div class="keyframe-greetings"> 
-          Changing Greetings Here
-      </div>
-    </span>
-    </div> */}
     </>
   );
 }
