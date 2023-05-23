f4e0982340df45b2eb16df6b6c63e09604e03dc0
William Moses <wmoses5k@gmail.com>
2023-05-23 15:35:47 +0100

    Updated BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar.js';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/HomePage/Home.js';
 import Portfolio from './components/PortfolioPage/Portfolio.js';
@@ -10,15 +12,24 @@ import PCG from './components/PCGPage/PCG.js'
 function App() {
   return (
     <>
-    <Router>
+    {/* <HashRouter>
       <Navbar />
-      <Routes>
-        <Route path="/Home" element={<Home/>} />
-        <Route path="/Portfolio" element={<Portfolio/>} />
-        <Route path="/About" element={<About/>} />
-        <Route path="/PCG" element={<PCG/>} />
-      </Routes>
-    </Router>
+        <Routes>
+          <Route path="/Home" element={<Home/>} />
+          <Route path="/Portfolio" element={<Portfolio/>} />
+          <Route path="/About" element={<About/>} />
+          <Route path="/PCG" element={<PCG/>} />
+        </Routes>
+    </HashRouter> */}
+    <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path="/Home" element={<Home/>} />
+          <Route path="/Portfolio" element={<Portfolio/>} />
+          <Route path="/About" element={<About/>} />
+          <Route path="/PCG" element={<PCG/>} />
+        </Routes>
+    </BrowserRouter>
     </>
   );
 }
