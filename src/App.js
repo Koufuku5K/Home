c81fdb092a8da9825f8301eb9a99fe321c585e85
William Moses <wmoses5k@gmail.com>
2023-05-23 19:01:06 +0100

    Changed BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar.js';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/HomePage/Home.js';
 import PortfolioPage from './components/PortfolioPage/PortfolioPage.js';
