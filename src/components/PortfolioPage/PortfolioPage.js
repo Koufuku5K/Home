47fea8ad4d343732599d933d7e2a99c2a11e4559
William Moses <wmoses5k@gmail.com>
2024-02-17 13:14:25 +0900

    Added Clips of Videos to Portfolio Page

--- src/components/PortfolioPage/PortfolioPage.js
+++ src/components/PortfolioPage/PortfolioPage.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 import { useEffect } from 'react'
 import {useTranslation} from 'react-i18next'
 import pcg_video from '../../videos/PCG_Video.mp4'
+import flashdodge_video from '../../videos/FlashDodgeGameClip.mp4'
+import initialtruth_video from '../../videos/InitialTruthGame.mp4'
 
 function PortfolioPage() {
   useEffect(() => {
@@ -37,7 +39,7 @@ function PortfolioPage() {
         <div class='videoBg'>
           <div class='pcgVideoDiv'>
             <h1 class="videoTitle">{t("portfolio1.title")}</h1>
-            <video id='pcgVideoAutoplay' class='pcgVideo' preload='auto' autoPlay muted loop controls>
+            <video class='pcgVideo' preload='auto' autoPlay muted loop controls>
                 <source src={pcg_video} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
@@ -55,7 +57,17 @@ function PortfolioPage() {
             <button><Link to="/PCG">Click Me!</Link></button>
         </div>
 
-        <div class='section'>
+        <div class='videoBg'>
+          <div class='flashdodgeVideoDiv'>
+            <h1 class="videoTitle">{t("portfolio2.title")}</h1>
+            <video class='flashdodgeVideo' preload='auto' autoPlay muted loop controls>
+                <source src={flashdodge_video} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+          </div>
+        </div>
+
+        <div id='flashdodgeDiv' class='section'>
           <h1 class='sectionTitle'>{t("portfolio2.title")}</h1>
           <br></br>
           <p>
@@ -66,7 +78,17 @@ function PortfolioPage() {
           <button><Link to="/FlashDodge">Click Me!</Link></button>
         </div>
 
-        <div class='section'>
+        <div class='videoBg'>
+          <div class='initialtruthVideoDiv'>
+            <h1 class="videoTitle">{t("portfolio3.title")}</h1>
+            <video class='initialtruthVideo' preload='auto' autoPlay muted loop controls>
+                <source src={initialtruth_video} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+          </div>
+        </div>
+
+        <div id='initialtruthDiv' class='section'>
           <h1 class='sectionTitle'>{t("portfolio3.title")}</h1>
           <br></br>
           <p>
